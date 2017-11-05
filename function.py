2f02ad00229ea132499596fe642c38f92beff622
ppedrycz <piotrekpedrycz@gmail.com>
2017-11-05 19:30:18 +0100

    Decomposition + debugging

---
+++ function.py
@@ -0,0 +1,40 @@
+import random
+
+from keras.layers import LSTM, Dense, Activation, Dropout
+from keras.models import Sequential
+
+from data import get_training_and_test_data
+
+from functools import partial
+
+# training data
+x, y = get_training_and_test_data()
+
+# cache for keras output
+cache = {}
+
+# minimized function
+def optimize(params):
+    current_params = (params[0] * 20, params[1] * 20)
+    print
+    print("Optimizing function for parameters: " + str(current_params))
+
+    if current_params not in cache:
+        model = Sequential()
+        model.add(LSTM(units=current_params[0], return_sequences=True, input_shape=(x.shape[1], x.shape[2])))
+        model.add(Dropout(0.2))
+        model.add(LSTM(current_params[1]))
+        model.add(Dropout(0.2))
+        model.add(Dense(y.shape[1]))
+        model.add(Activation("softmax"))
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+        cache[current_params] = [model.fit(x, y, epochs=1, batch_size=32, validation_split=0.15).history.get('val_acc')[-1]]
+
+    print("result: " + str(cache[current_params]))
+    return cache[current_params]
+
+# function parameters
+attr_1 = partial(random.randint, 1, 5)
+attr_2 = partial(random.randint, 1, 5)
+attrs = [attr_1, attr_2]
+
---
+++ optimizer_genetic.py
@@ -0,0 +1,41 @@
+from deap import creator, base, tools, algorithms
+
+from function import optimize, attrs, cache
+
+# evolutionary tools
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+# evolutionary parameters
+population_size = 10
+generations = 10
+crossover_probability = 0.05
+mating_probability = 0.2
+mutation_probability = 0.05
+tournament_size = 2
+toolbox = base.Toolbox()
+toolbox.register("individual", tools.initCycle, creator.Individual, attrs)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("evaluate", optimize)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=crossover_probability)
+toolbox.register("select", tools.selTournament, tournsize=tournament_size)
+
+# create population and run algorithm
+population = toolbox.population(n=population_size)
+for gen in range(generations):
+    print
+    print("GENRATION " + str(gen))
+    offspring = algorithms.varAnd(population, toolbox, cxpb=mating_probability, mutpb=mutation_probability)
+    fits = toolbox.map(toolbox.evaluate, offspring)
+    for fit, ind in zip(fits, offspring):
+        ind.fitness.values = fit
+    population = toolbox.select(offspring, k=len(population))
+
+# get best population representative
+top1 = tools.selBest(population, k=1)[0]
+
+# print best representative
+print
+print("Best result for parameters: " + str(top1))
+
