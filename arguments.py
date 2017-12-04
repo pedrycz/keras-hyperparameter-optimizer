0f0f03191a8535b7cd271a02384379e5e7a43d1c
Piotr Pedrycz <piotrekpedrycz@gmail.com>
2017-12-04 09:54:06 +0100

    Extracted deap parameters to arguments (#1)

--- arguments.py
+++ arguments.py
@@ -6,5 +6,11 @@ parser.add_argument('--data-fraction', dest='data_fraction', default=0.46, type=
 parser.add_argument('--test-fraction', dest='test_fraction', default=0.1, type=float)
 parser.add_argument('--epochs', dest='epochs', default=25, type=int)
 parser.add_argument('--early-stopping-patience', dest='early_stopping_patience', default=5, type=int)
+parser.add_argument('--population-size', dest='population_size', default=10, type=int)
+parser.add_argument('--generations', dest='generations', default=5, type=int)
+parser.add_argument('--crossover-probability', dest='crossover_probability', default=0.1, type=float)
+parser.add_argument('--manting-probability', dest='manting_probability', default=0.3, type=float)
+parser.add_argument('--mutation-probability', dest='mutation_probability', default=0.1, type=float)
+parser.add_argument('--tournament-size', dest='tournament_size', default=2, type=int)
 
 args = parser.parse_args()
--- optimizer_genetic.py
+++ optimizer_genetic.py
@@ -2,6 +2,7 @@ import time
 from deap import creator, base, tools, algorithms
 
 from function import optimize, attrs
+from arguments import args
 
 start = time.time()
 
@@ -10,27 +11,21 @@ creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 # evolutionary parameters
-population_size = 10
-generations = 5
-crossover_probability = 0.1
-mating_probability = 0.3
-mutation_probability = 0.1
-tournament_size = 2
 toolbox = base.Toolbox()
 toolbox.register("individual", tools.initCycle, creator.Individual, attrs)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("evaluate", optimize)
 toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=crossover_probability)
-toolbox.register("select", tools.selTournament, tournsize=tournament_size)
+toolbox.register("mutate", tools.mutFlipBit, indpb=args.crossover_probability)
+toolbox.register("select", tools.selTournament, tournsize=args.tournament_size)
 
 # create population and run algorithm
-population = toolbox.population(n=population_size)
+population = toolbox.population(n=args.population_size)
 
-for gen in range(generations):
+for gen in range(args.generations):
     print
     print("GENRATION " + str(gen))
-    offspring = algorithms.varAnd(population, toolbox, cxpb=mating_probability, mutpb=mutation_probability)
+    offspring = algorithms.varAnd(population, toolbox, cxpb=args.manting_probability, mutpb=args.mutation_probability)
     fits = toolbox.map(toolbox.evaluate, offspring)
     for fit, ind in zip(fits, offspring):
         ind.fitness.values = fit
